5ee3225854571de4ffd48793252f8d73ec3596c4
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2021-10-26 21:53:37 +0300

    Add files via upload

---
+++ src/client/pages/Components/Components/quizDataMath.js
@@ -0,0 +1,36 @@
+export const quizDataMath = [
+  {
+    id: 0,
+    question: ` 2*6`,
+    options: [`3`, `12`, `5`],
+    answer: `12`
+  },
+  {
+    id: 1,
+    question: `1000/2`,
+    options: [`500`, `100`, `20`],
+    answer: `500`
+  },
+  {
+    id: 3,
+    question: `300+400`,
+    options: [`600`, `200`, `700`],
+    answer: `700`
+  },
+  {
+    id: 4,
+    question: `5*3`,
+    options: [`12`, `17`, `15`],
+    answer: `15`
+  },
+  {
+    id: 5,
+    question: `200-11`,
+    options: [`89`, `189`, `191`],
+    answer: `189`
+  }
+  
+  
+  
+  
+];
