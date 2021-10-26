889fda23a729b63437bc8f3347ba913c0345893a
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2021-10-26 21:51:00 +0300

    Add files via upload

---
+++ src/client/services/apiService.js
@@ -0,0 +1,12 @@
+  
+  export const User = {
+    update: (id, data) => {
+        fetch('/api/users/' + id, {
+            method: 'PUT', 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          body: JSON.stringify(data)
+        }) 
+    }
+}
